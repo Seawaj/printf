1fe0329dba66426cf6ab88011f4bd4e11ee927f0
Seawaj <122825480+Seawaj@users.noreply.github.com>
2023-03-27 16:52:27 +0100

    Update active_flags.c

--- active_flags.c
+++ active_flags.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* get_flags - To calculates active flags
+* get_flags - To calculates active flags.
 *
 * @format: Formatted string in which to print the arguments
 * @i: take a parameter.
