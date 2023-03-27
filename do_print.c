8bfc8aa1cfbdf3f1b4ac192b7dbbefde49248f2b
Seawaj <122825480+Seawaj@users.noreply.github.com>
2023-03-27 16:52:58 +0100

    Update do_print.c

--- do_print.c
+++ do_print.c
@@ -3,10 +3,10 @@
 /**
 * handle_print - to print an argument based on its type
 *
-* @fmt: Formatted string in which to print the arguments.
-* @list: List of arguments to be printed.
+* @fmt: Formatted string in which to print the arguments
+* @list: List of arguments to be printed
 * @ind: ind.
-* @buffer: Buffer array to handle print.
+* @buffer: Buffer array to handle print
 * @flags: Calculates active flags
 * @width: get width.
 * @precision: Precision specification
