80151c972dfb06090dd50943c572ea48469261ca
FADILAT YUSUF <yusuffadilat2000@gmail.com>
2023-03-27 16:00:21 +0100

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_unsigned - Prints an unsigned number
+* print_unsigned - This prints an unsigned number
 *
 * @types: List a of arguments
 * @buffer: Buffer array to handle print
